8acc1074bff348fcefcb2dff74aad7e3be6485a4
Molinet, Fabien <fabien.molinet@hager.fr>
2015-06-01 18:53:04 +0200

    Demo delegate bug that makes everything buggy

---
+++ samples/PSPDFCatalog/PSPDFCatalog/Catalog/Customization/SimplePDFViewControllerDelegateExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PSPDFKit;
+using Foundation;
+
+namespace PSPDFCatalog.Catalog.Customization
+{
+	public class SimplePDFViewControllerDelegateExample: PSPDFViewControllerDelegate
+	{
+		public override void DidShowPageView(PSPDFViewController pdfController, PSPDFPageView pageView)
+		{
+			base.DidShowPageView(pdfController, pageView);
+		}
+
+		public override bool ShouldShowController(PSPDFViewController pdfController, IPSPDFPresentableViewController controller, IPSPDFHostableViewController hostController, NSDictionary options, bool animated)
+		{
+			return base.ShouldShowController(pdfController, controller, hostController, options, animated);
+		}
+	}
+}
+
